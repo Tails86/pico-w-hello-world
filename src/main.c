dbf025dff2544ddc84bb68b00afcbc9ab232e2ee
James Smith <jmsmith86@gmail.com>
2024-04-14 13:22:51 -0600

    Replaced 1 and 0 with true and false

--- src/main.c
+++ src/main.c
@@ -7,18 +7,18 @@ int main()
     // Initialize the cyw43
     cyw43_arch_init();
     // The LED is hooked into the CYW43, so it's necessary to go through CYW43 to activate it
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
     // Initialize stdio through USB
     stdio_usb_init();
 
     while (true)
     {
         printf("Hello\n");
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
         sleep_ms(1000);
 
         printf("World\n");
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
         sleep_ms(1000);
     }
     return 0;
